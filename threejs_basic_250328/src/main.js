ff36d5b522ece1ae0bb1ac508c9781ceec1635b5
happy-ko-kamakura <sachikonovel@yahoo.co.jp>
2025-03-28 23:45:07 +0900

    Orbit Controls

--- threejs_basic_250328/src/main.js
+++ threejs_basic_250328/src/main.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 
 init()
 async function init() {  // init関数内で非同期処理をawaitで待てる
@@ -17,14 +19,19 @@ async function init() {  // init関数内で非同期処理をawaitで待てる
 
   // ジオメトリ
   // const geometry = new THREE.BoxGeometry(); // 立方体
-  const geometry = new THREE.PlaneGeometry(20, 10);
+  // const geometry = new THREE.PlaneGeometry(20, 10);
   // const geometry = new THREE.PlaneGeometry(10, 10, 5, 2); // 平面 (width, height, widthSegments: 平面横方向のセグメント数, heightSegments: 平面縦方向のセグメント数)
   // const geometry = new THREE.SphereGeometry(5,5) // 球体 (radius: 半径, widthSegments: 横のセグメント数, heightSegments: 縦のセグメント数, ...);
   // const geometry = new THREE.TorusGeometry(5, 1, 50, 50) // ドーナツ型 (radius: 半径, tube: 太さ, radialSegments: （ドーナツの輪のセグメント数） オブジェクトの中心から外側に向かう放射状のセグメントの数, tubularSegments: チューブのセグメント数） オブジェクトのチューブの周りのセグメントの数)
+  const geometry = new THREE.TorusGeometry(5, 2, 50, 50);
+
 
   const texLoader = new THREE.TextureLoader(); // テクスチャを画像から作成する場合はTextureLoaderを使用
   const texture1 = await texLoader.loadAsync("https://static.not-equal.dev/ja_webgl_basic/img/output1.jpg");
-  const texture2 = await texLoader.loadAsync("https://static.not-equal.dev/ja_webgl_basic/img/output2.jpg");
+  const video = document.querySelector('#video-texture');
+//  const texture1 = new THREE.VideoTexture(video);
+//  console.log(texture1)
+  // const texture2 = await texLoader.loadAsync("https://static.not-equal.dev/ja_webgl_basic/img/output2.jpg");
 
 
 
@@ -46,9 +53,9 @@ async function init() {  // init関数内で非同期処理をawaitで待てる
 
   // const material = new THREE.MeshBasicMaterial({ map: texture1 }); //マテリアルへのテクスチャ設定
   const material = new THREE.MeshBasicMaterial({ map: texture1 });
-    setTimeout(() => {
-    material.map = texture2;
-    }, 2000);
+    // setTimeout(() => {
+    // material.map = texture2;
+    // }, 2000);
   
 
   // material.color = new THREE.Color( 0x00ff00 );
@@ -68,7 +75,14 @@ async function init() {  // init関数内で非同期処理をawaitで待てる
 
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
+
+  const control = new OrbitControls(camera, renderer.domElement);
+  control.autoRotate = true;
+  control.enableDamping = true;
+
+
   camera.position.z = 15;
+
 
   let i = 0;
   function animate() {
@@ -76,6 +90,8 @@ async function init() {  // init関数内で非同期処理をawaitで待てる
 
     // cube.rotation.x += 0.01;
     // cube.rotation.y += 0.01;
+
+    control.update();
 
     renderer.render(scene, camera);
   }
