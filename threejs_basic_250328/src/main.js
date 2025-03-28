16adbfd1a02827c72949ea7723d5135f34a3741f
happy-ko-kamakura <sachikonovel@yahoo.co.jp>
2025-03-28 22:47:02 +0900

    basic material

--- threejs_basic_250328/src/main.js
+++ threejs_basic_250328/src/main.js
@@ -14,14 +14,25 @@ document.body.appendChild(renderer.domElement);
 
 // ジオメトリ
 // const geometry = new THREE.BoxGeometry(); // 立方体
-// const geometry = new THREE.PlaneGeometry(10, 10, 5, 2); // 平面 (width, height, widthSegments: 平面横方向のセグメント数, heightSegments: 平面縦方向のセグメント数)
+const geometry = new THREE.PlaneGeometry(10, 10, 5, 2); // 平面 (width, height, widthSegments: 平面横方向のセグメント数, heightSegments: 平面縦方向のセグメント数)
 // const geometry = new THREE.SphereGeometry(5,5) // 球体 (radius: 半径, widthSegments: 横のセグメント数, heightSegments: 縦のセグメント数, ...);
-const geometry = new THREE.TorusGeometry(5, 1, 50, 50) // ドーナツ型 (radius: 半径, tube: 太さ, radialSegments: （ドーナツの輪のセグメント数） オブジェクトの中心から外側に向かう放射状のセグメントの数, tubularSegments: チューブのセグメント数） オブジェクトのチューブの周りのセグメントの数)
+// const geometry = new THREE.TorusGeometry(5, 1, 50, 50) // ドーナツ型 (radius: 半径, tube: 太さ, radialSegments: （ドーナツの輪のセグメント数） オブジェクトの中心から外側に向かう放射状のセグメントの数, tubularSegments: チューブのセグメント数） オブジェクトのチューブの周りのセグメントの数)
 
 const material = new THREE.MeshBasicMaterial({
   color: "#fafafa",
-  wireframe: true // true: セグメントを視覚的に確認
+  side: THREE.DoubleSide,
+  transparent: true,
+  opacity: .5,
+  // alphaTest: .5  // アルファブレンディングのパフォーマンスを良くしたい場合は0.5に設定がベスト
+
+  // wireframe: true // true: セグメントを視覚的に確認
 });
+
+// material.wireframe = true
+setTimeout(() => { // 1秒後にwireframe適用
+  material.wireframe  = true
+}, 1000)
+
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 camera.position.z = 15;
