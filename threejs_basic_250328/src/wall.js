0a209108b91bd08505ebe77cfa3b6d4896684c83
happy-ko-kamakura <sachiko.dev.0811@gmail.com>
2025-04-13 00:50:59 +0900

    複数の立方体

--- threejs_basic_250328/src/wall.js
+++ threejs_basic_250328/src/wall.js
@@ -25,53 +25,70 @@ async function init() {
 	antialias: true,
   });
   renderer.setSize(window.innerWidth, window.innerHeight);
+  // Three.jsの影のレンダリングの有効化
   renderer.shadowMap.enabled = true;
   document.body.appendChild(renderer.domElement);
 
   const axis = new THREE.AxesHelper(100);
-  axis.visible = false;
+  axis.visible = false; // ヘルパーの非表示
   scene.add(axis);
+  //04: Ambientライトの追加
   const amLight = new THREE.AmbientLight(0x3f3f46, 0.6);
   scene.add(amLight);
 
+  //05: Pointライトの追加
   const pLight = new THREE.PointLight(0xffffff, 80, 300, 1);
   pLight.position.set(-26, 7, 50);
   const pHelper = new THREE.PointLightHelper(pLight, 3);
-  pHelper.visible = true;
+  pHelper.visible = false; // ヘルパーの非表示
 
   scene.add(pLight, pHelper);
 
+  // 影をつくるライトの有効化
   pLight.castShadow = true;
+  // ↓負荷大になる可能性あり
   pLight.shadow.mapSize.width = 1024;
   pLight.shadow.mapSize.height = 1024;
 
+  //06: Directionalライトの追加
   const dLight = new THREE.DirectionalLight(0xaabbff, 2);
   dLight.position.set(0, 0, 1);
   const dHelper = new THREE.DirectionalLightHelper(dLight, 3);
-  dHelper.visible = false;
+  dHelper.visible = false; // ヘルパーの非表示
 
   scene.add(dLight, dHelper);
-  const X_NUM = 10;
-  const Y_NUM = 6;
+
+  //07: 定数の定義(メッシュ配置)
+  const X_NUM = 10; //横軸
+  const Y_NUM = 6;  //縦軸
+  //01: 定数の定義(メッシュ作成準備)
   const SCALE = 30;
   const COLORS = { MAIN: "#f3f4f6", SUB: "#60a5fa" };
 
+  //02: ジオメトリの作成
   const boxGeo = new THREE.BoxGeometry(SCALE, SCALE, SCALE);
 
+  //03: マテリアルの作成
   const mainMate = new THREE.MeshLambertMaterial({ color: COLORS.MAIN });
   const subMate = mainMate.clone();
   subMate.color.set(COLORS.SUB);
 
+  //08: メッシュの作成
   const boxes = [];
 
   for (let y = 0; y < Y_NUM; y++) {
 	for (let x = 0; x < X_NUM; x++) {
 	  const material = Math.random() < 0.2 ? subMate : mainMate;
+      // 作成したジオメトリと組み合わせてメッシュを作成し、boxesに追加
 	  const box = new THREE.Mesh(boxGeo, material);
 	  boxes.push(box);
+      // メッシュの配置
+    //  box.position.x = x * SCALE;
+    //  box.position.y = y * SCALE;
 	  box.position.x = x * SCALE - (X_NUM * SCALE) / 2;
 	  box.position.y = y * SCALE - (Y_NUM * SCALE) / 2;
 	  box.position.z = mapRand(-10, 10);
+      // メッシュ間のギャップの追加
 	  box.scale.set(0.98, 0.98, 0.98);
 	  box.castShadow = true;
 	  box.receiveShadow = true;
